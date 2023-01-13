f6e322b8fc8ce62b0587878229ad2c82a7506c03
raul_ <raul_nunes07@hotmail.com>
2023-01-12 21:18:49 -0300

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rent [] vect = new Rent[10];
+        System.out.print("How many room will be rented? :");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println();
+            System.out.println("Rent #" + i + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.next();
+            System.out.print("Room: ");
+            int roomNumber=sc.nextInt();
+
+
+            vect [roomNumber] = new Rent(name, email);
+
+        }
+
+        sc.close();
+    }
+}
